873891e0e27b4ee7acfb7c55d689f90e01d6f350
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-16 17:34:49 +0900

    Attempt to fix single-db linking error

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   # When you delete a list, you should delete all associated bookmarks
-  self.table_name = 'wlist_lists'
+  self.table_name = wlist_lists
   has_many :bookmarks, dependent: :destroy, foreign_key: 'wlist_list_id'
   has_many :movies, through: :bookmarks, foreign_key: 'wlist_list_id'
   has_one_attached :photo, foreign_key: 'wlist_list_id'
