38f8e47d20b1af290a904be905904544a3c7f271
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-16 17:12:05 +0900

    Modified for single-db

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movie
-  belongs_to :list
+  self.table_name = 'wlist_bookmarks'
+  belongs_to :movie, foreign_key: 'wlist_movie_id'
+  belongs_to :list, foreign_key: 'wlist_list_id'
 
   validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id, message: 'has already been bookmarked in this list' }
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,9 @@
 class List < ApplicationRecord
   # When you delete a list, you should delete all associated bookmarks
-  has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks
-  has_one_attached :photo
+  self.table_name = 'wlist_lists'
+  has_many :bookmarks, dependent: :destroy, foreign_key: 'wlist_list_id'
+  has_many :movies, through: :bookmarks, foreign_key: 'wlist_list_id'
+  has_one_attached :photo, foreign_key: 'wlist_list_id'
 
   validates :name, presence: true, uniqueness: true
 end
