2123f1d99d1a847b19c291a6fbd4b712816eba7d
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-16 18:07:23 +0900

    Deleted user model for single-db

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  self.table_name = wlist_users
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
